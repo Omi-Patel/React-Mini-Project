6c983926510e855562abcbbc663c81aa9494055a
Omi-Patel <omipatel7113@gmail.com>
2023-04-22 01:40:22 +0530

    First React commit

---
+++ src/Cards.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Cards(props) {
+    return (
+        <>
+            <div className='cards'>
+                <div className='card'>
+                    <img src={props.img_src} width="400px" alt='This is Image' className='img_class'
+                    />
+                    <div className='card_info'>
+                        <span >{props.title}</span>
+                        <h3>{props.sname}</h3>
+                        <a href='' target='_blank'>
+                            <button>Watch Now!</button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Cards;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,63 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Cards from './Cards'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+
+    <h1 className='headingup'> Top NetFlix Series </h1>
+    <div className='allcards'>
+      <Cards
+        img_src="https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20200813125652/dark-1.jpg"
+        title="An Original NetFlix"
+        sname="DARK"
+        link=""
+      />
+
+      <Cards
+        img_src="https://static.toiimg.com/photo/97276790.cms?resizemode=4"
+        title="An Original NetFlix"
+        sname="SOUTH"
+        link=""
+      />
+
+      <Cards
+        img_src="https://www.koimoi.com/wp-content/new-galleries/2023/01/pathaan-quick-review-001.jpg"
+        title="An Original NetFlix"
+        sname="PATHAN"
+        link=""
+      />
+
+    </div>
+
+    <div className='allcards'>
+
+    <Cards
+        img_src="https://www.koimoi.com/wp-content/new-galleries/2023/01/pathaan-quick-review-001.jpg"
+        title="An Original NetFlix"
+        sname="PATHAN"
+        link=""
+      />
+
+      <Cards
+        img_src="https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20200813125652/dark-1.jpg"
+        title="An Original NetFlix"
+        sname="DARK"
+        link=""
+      />
+
+      <Cards
+        img_src="https://static.toiimg.com/photo/97276790.cms?resizemode=4"
+        title="An Original NetFlix"
+        sname="SOUTH"
+        link=""
+      />
+
+    </div>
+
+    {/* <App /> */}
   </React.StrictMode>
 );
 
