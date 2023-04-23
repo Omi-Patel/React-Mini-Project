55db4eaf6b1c9700c2b9688d693fe38d13114df3
Omi-Patel <omipatel7113@gmail.com>
2023-04-23 21:47:46 +0530

    changing index

---
+++ src/Sdata.jsx
@@ -0,0 +1,57 @@
+const Sdata = [
+
+    {
+        id: 1,
+        img_src: "https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20200813125652/dark-1.jpg",
+        title: "An Original NetFlix",
+        sname: "DARK",
+        link: ""
+    },
+
+    {
+        id: 2,
+        img_src:"https://static.toiimg.com/photo/97276790.cms?resizemode=4",
+        title:"An Original NetFlix",
+        sname:"SOUTH",
+        link:""
+    },
+
+    {
+        id: 3,
+        img_src:"https://www.koimoi.com/wp-content/new-galleries/2023/01/pathaan-quick-review-001.jpg",
+        title:"An Original NetFlix",
+        sname:"PATHAN",
+        link:""
+    }
+]
+
+const S2data = [
+
+    {
+        id: 1,
+        img_src:"https://s.studiobinder.com/wp-content/uploads/2020/12/All-Marvel-Movies-in-Order-of-Release-StudioBinder.jpg",
+        title:"Marvel Studio",
+        sname:"AVENGERS INFINITY WAR",
+        link:""
+    },
+
+    {
+        id: 2,
+        img_src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrVF5T8wW9QkOJnf2g9jrcCKZsOcYIMFFql6cBSSU7xQ&usqp=CAU&ec=48665699",
+        title:"Marvel Original Studio",
+        sname:"GUARDIANS OF THE GALAXY",
+        link:""
+    },
+
+    {
+        id: 3,
+        img_src:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQJoJ114ADEzNvr44ijPzEAuNTrrO9dpgTwc0IcOWCA&usqp=CAU&ec=48665699",
+        title:"Amazon Prime Video",
+        sname:"ANEMY",
+        link:""
+    }
+
+]
+
+export default Sdata;
+export {S2data};
--- src/index.js
+++ src/index.js
@@ -3,62 +3,35 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Cards from './Cards'
+import Cards from './Cards';
+import Sdata, {S2data} from './Sdata';
+
+
+function ncard(val) {
+  return (
+    <Cards
+      key={val.id}
+      img_src={val.img_src}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <>
+      <h1 className='headingup'> Top NetFlix Series </h1>
+      <div className='allcards'>
+        {Sdata.map(ncard)}
+      </div>
 
-    <h1 className='headingup'> Top NetFlix Series </h1>
-    <div className='allcards'>
-      <Cards
-        img_src="https://d2rdhxfof4qmbb.cloudfront.net/wp-content/uploads/20200813125652/dark-1.jpg"
-        title="An Original NetFlix"
-        sname="DARK"
-        link=""
-      />
-
-      <Cards
-        img_src="https://static.toiimg.com/photo/97276790.cms?resizemode=4"
-        title="An Original NetFlix"
-        sname="SOUTH"
-        link=""
-      />
-
-      <Cards
-        img_src="https://www.koimoi.com/wp-content/new-galleries/2023/01/pathaan-quick-review-001.jpg"
-        title="An Original NetFlix"
-        sname="PATHAN"
-        link=""
-      />
-
-    </div>
-
-    <div className='allcards'>
-
-    <Cards
-        img_src="https://s.studiobinder.com/wp-content/uploads/2020/12/All-Marvel-Movies-in-Order-of-Release-StudioBinder.jpg"
-        title="Marvel Studio"
-        sname="AVENGERS INFINITY WAR"
-        link=""
-      />
-
-      <Cards
-        img_src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrVF5T8wW9QkOJnf2g9jrcCKZsOcYIMFFql6cBSSU7xQ&usqp=CAU&ec=48665699"
-        title="Marvel Original Studio"
-        sname="GUARDIANS OF THE GALAXY"
-        link=""
-      />
-
-      <Cards
-        img_src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTzQJoJ114ADEzNvr44ijPzEAuNTrrO9dpgTwc0IcOWCA&usqp=CAU&ec=48665699"
-        title="Amazon Prime Video"
-        sname="ANEMY"
-        link=""
-      />
-
-    </div>
-
+      <div className='allcards'>
+      {S2data.map(ncard)}
+      </div>
+    </>
     {/* <App /> */}
   </React.StrictMode>
 );
